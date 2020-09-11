770cb5b2d15741990bc95af0b9fa060b8e1ca6e9
dianlujitao <dianlujitao@lineageos.org>
2020-09-11 11:41:51 +0800

    soong: Hack to preserve cyclone product variables
    Change-Id: Ice4093b86baed6974bab630f0e80f6b13a5e28f3

--- android/variable.go
+++ android/variable.go
@@ -584,6 +584,11 @@ func createVariableProperties(moduleTypeProps []interface{}, productVariables in
 func createVariablePropertiesType(moduleTypeProps []interface{}, productVariables interface{}) reflect.Type {
 	typ, _ := proptools.FilterPropertyStruct(reflect.TypeOf(productVariables),
 		func(field reflect.StructField, prefix string) (bool, reflect.StructField) {
+			if strings.HasPrefix(prefix, "Product_variables.Cyclone") {
+				// Convert Product_variables.Cyclone.Foo to Cyclone.Foo
+				_, prefix = splitPrefix(prefix)
+			}
+
 			// Filter function, returns true if the field should be in the resulting struct
 			if prefix == "" {
 				// Keep the top level Product_variables field
